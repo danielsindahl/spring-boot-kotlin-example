e55c99d3fca4222ddbbdfa6b98640e019804528f
Daniel Sindahl <daniel.sindahl@assessio.se>
2017-01-18 09:39:40 +0100

    Some cleanup

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -5,7 +5,4 @@ import org.springframework.data.annotation.PersistenceConstructor
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "user")
-data class User @PersistenceConstructor constructor(
-        @Id val id: String? = null,
-        val userName: String
-)
+data class User @PersistenceConstructor constructor(@Id val id: String? = null, val userName: String? = null)
